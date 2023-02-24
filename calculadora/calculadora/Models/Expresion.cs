9786ff6f19e07f587cdad8660e7b50b17273f8c4
cenamoradoceutec <cenamorado@unitec.edu>
2023-02-23 20:38:23 -0600

    secon commit

---
+++ calculadora/calculadora/Models/Expresion.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace calculadora.Models
+{
+    public class Expresion
+    {
+        public decimal Numero1 { get; set; }
+        public decimal Numero2 { get; set; }
+
+
+
+        public decimal Suma()
+        {
+            return Numero1 + Numero2;
+        }
+        public decimal Resta()
+        {
+            return Numero1 - Numero2;
+        }
+        public decimal Division()
+        {
+            return Numero1 / Numero2;
+        }
+        public decimal Multiplicacion()
+        {
+            return Numero1 * Numero2;
+        }
+
+
+        public  string Sumar()
+        {
+            return $"La Suma de los numeros es: {Suma()} ";
+        }
+        public string Restar()
+        {
+            return $"La resta de los numeros es: {Resta()} ";
+        }
+        public string Dividir()
+        {
+            return $"La division de los numeros es: {Division()} ";
+        }
+        public string Multiplicar()
+        {
+            return $"La multiplicacion de los numeros es: {Multiplicacion()} ";
+        }
+
+        public override string ToString()
+        {
+            return $"Hola mi nombre es y mi edad es: {Suma()} ";
+        }
+
+        
+    }
+}
---
+++ calculadora/calculadora/ViewModel/ViewModelCalculadora.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using Xamarin.Forms;
+using calculadora.Models;
+
+
+namespace calculadora.ViewModel
+{
+    public class ViewModelCalculadora : INotifyPropertyChanged
+    {
+        public ViewModelCalculadora()
+        {
+
+            Sumar = new Command(
+                () => {
+                    Expresion suma = new Expresion()
+                    {
+
+                        Numero1 = this.num1,
+                        Numero2 = this.num2,
+                            
+
+                    };
+                    Mensaje = suma.Sumar();
+                }
+            );
+
+            Restar = new Command(
+                () => {
+                    Expresion resta = new Expresion()
+                    {
+
+                        Numero1 = this.num1,
+                        Numero2 = this.num2,
+
+
+                    };
+                    Mensaje = resta.Restar();
+                }
+            );
+
+            Multiplicar = new Command(
+                () => {
+                    Expresion multiplicar = new Expresion()
+                    {
+
+                        Numero1 = this.num1,
+                        Numero2 = this.num2,
+
+
+                    };
+                    Mensaje = multiplicar.Multiplicar();
+                }
+            );
+
+            Dividir = new Command(
+                () => {
+                    Expresion dividir = new Expresion()
+                    {
+
+                        Numero1 = this.num1,
+                        Numero2 = this.num2,
+
+
+                    };
+                    Mensaje = dividir.Dividir();
+                }
+            );
+
+
+        }
+
+        string mensaje;
+
+        public string Mensaje
+        {
+
+            get => mensaje;
+            set
+            {
+
+                mensaje = value;
+                var arg = new PropertyChangedEventArgs(nameof(Mensaje));
+                PropertyChanged?.Invoke(this, arg);
+
+            }
+
+        }
+
+        decimal num1;
+
+        public decimal Num1
+        {
+
+            get => num1;
+            set
+            {
+                num1 = value;
+                var arg = new PropertyChangedEventArgs(nameof(num1));
+                PropertyChanged?.Invoke(this, arg);
+            }
+        }
+
+        decimal num2;
+
+        public decimal Num2
+        {
+
+            get => num2;
+            set
+            {
+                num2 = value;
+                var arg = new PropertyChangedEventArgs(nameof(num2));
+                PropertyChanged?.Invoke(this, arg);
+            }
+        }
+
+        public Command Sumar { get; }
+        public Command Restar { get; }
+        public Command Multiplicar { get; }
+        public Command Dividir { get; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
